f8ff788ae7be07fd5195ae29e1ae04f96cbdc38b
amitk <amitkazado@gmail.com>
2023-01-11 20:40:15 +0200

    Created Ball game object, added "bouncing" effect to the ball on the screen.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import time
 from turtle import Screen
 from Ball import Ball
 from Paddle import Paddle
 from Scoreboard import Scoreboard
 
+TOP_WALL = 280
+BOTTOM_WALL = -280
+LEFT_WALL = -310
+RIGHT_WALL = 310
 
 screen = Screen()
-
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+
 ball = Ball()
+
 l_paddle = Paddle(-280, 0)
 r_paddle = Paddle(280, 0)
+
 l_scoreboard = Scoreboard(-120, 260)
 r_scoreboard = Scoreboard(120, 260)
 
@@ -24,8 +31,25 @@ screen.onkeypress(r_paddle.move_down, "Down")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
     ball.move()
 
+    if ball.ycor() > TOP_WALL or ball.ycor() < BOTTOM_WALL:
+        ball.change_direction()
+    
+    # requires fixing
+    # if ball.distance(l_paddle) < 50 and ball.xcor() < -260 or ball.distance(r_paddle) < 50 and ball.xcor() > 260:
+    #     ball.change_direction()
+
+    if ball.xcor() < LEFT_WALL:
+        time.sleep(1)
+        l_scoreboard.increase_score()
+        ball.restart_ball()
+
+    if ball.xcor() > RIGHT_WALL:
+        time.sleep(1)
+        r_scoreboard.increase_score()
+        ball.restart_ball()
 
 screen.exitonclick()
